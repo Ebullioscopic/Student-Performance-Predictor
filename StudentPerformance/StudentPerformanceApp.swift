ea1897d71eb73b1b82488ffae3d5e4cb6df3aeba
Ebullioscopic <hrhn.mudaliar251@gmail.com>
2024-11-06 23:11:41 +0530

    Initial Commit

---
+++ StudentPerformance/StudentPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentPerformanceApp.swift
+//  StudentPerformance
+//
+//  Created by admin63 on 06/11/24.
+//
+
+import SwiftUI
+
+@main
+struct StudentPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
